d5fa91ec1f923c77c02152cc61630d4033f22c44
Sarge <sercan.altundas@gmail.com>
2018-07-24 17:29:09 +0300

    [feature] model importer for test game object

--- Assets/Scripts/ModelImporter.cs
+++ Assets/Scripts/ModelImporter.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ModelImporter : MonoBehaviour {
+    
+    void Start()
+    {
+        Mesh holderMesh = new Mesh();
+        ObjImporter newMesh = new ObjImporter();
+        holderMesh = newMesh.ImportFile("C:/Users/Sarge/Desktop/test.obj");
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+
+        filter.mesh = holderMesh;
+        
+        Vector3 v = holderMesh.bounds.extents;
+        float max = Mathf.Max(new float[] { v.x, v.y, v.z });
+
+        transform.localScale = Vector3.one / max;
+
+        transform.Translate(holderMesh.bounds.center / max * -1);
+    }
 }
