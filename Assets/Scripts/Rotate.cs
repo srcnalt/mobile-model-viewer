007f85d0d9291916b4f0d83a04f8f64339297b7d
Sarge <sercan.altundas@gmail.com>
2018-07-24 17:28:09 +0300

    [feature] object rotation script

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public bool rotate;
+    
+    private void Update()
+    {
+        if (rotate) transform.Rotate(Vector3.up * 100 * Time.deltaTime);
+    }
+}
