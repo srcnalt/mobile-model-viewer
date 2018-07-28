ede8e751a3ec7db626efb5ccc7fc744ec64970c4
Sarge <sercan.altundas@gmail.com>
2018-07-28 14:30:42 +0300

    [refactor] removed model importer playground

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIController : MonoBehaviour {
+    public Transform UserLoginPanel;
+    public Transform ModelSelectPanel;
+    public Transform ModelViewPanel;
+    public Transform ModelStatsPanel;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Transform ActivePanel;
+
+    private void OnEnable()
+    {
+        ActivePanel = UserLoginPanel;
+        ActivePanel.gameObject.SetActive(true);
+
+        ModelSelectPanel.gameObject.SetActive(false);
+        ModelViewPanel.gameObject.SetActive(false);
+        ModelStatsPanel.gameObject.SetActive(false);
+    }
+
+    public void SwitchPanel(Transform nextPanel)
+    {
+        ActivePanel.gameObject.SetActive(false);
+        ActivePanel = nextPanel;
+        ActivePanel.gameObject.SetActive(true);
+    }
 }
