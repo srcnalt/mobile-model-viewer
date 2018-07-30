ecfda0bfbad40f9c0284eaa48f0ee33192a32414
Sarge <sercan.altundas@gmail.com>
2018-07-30 07:25:12 +0300

    [feature] options controller added

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    public Text statsText;
+    public Slider morphSlider;
+    public MeshRenderer meshRenderer;
+    private Material defaultMaterial;
+
+    public void SwitchShader(Material material)
+    {
+        meshRenderer.material = material;
+    }
+
+    public void SetToDefaultShader()
+    {
+        StartCoroutine(Co_LoadDefaultMaterial());
+    }
+
+    private IEnumerator Co_LoadDefaultMaterial()
+    {
+        while(defaultMaterial == null)
+        {
+            defaultMaterial = ModelLoader.material;
+
+            yield return null;
+        }
+
+        meshRenderer.material = defaultMaterial;
+    }
+
+    public void GetModelStats()
+    {
+        statsText.text = ObjImporter.modelStats;
+    }
+
+    public void ChangeMorphAmount()
+    {
+        meshRenderer.material.SetFloat("_Value", morphSlider.value);
+    }
+}
